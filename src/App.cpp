c3dd71a0dcc9b40b9d50e767d6a4af306df323b1
aydinguler <69461459+aydinguler@users.noreply.github.com>
2021-03-05 11:59:24 +0300

    Create App.cpp with main()

---
+++ src/App.cpp
@@ -0,0 +1,71 @@
+
+#include "AppComponent.hpp"
+
+#include "controller/WorkController.hpp"
+#include "controller/StaticController.hpp"
+
+#include "oatpp-swagger/Controller.hpp"
+
+#include "oatpp/network/Server.hpp"
+
+#include <iostream>
+
+void run() {
+  
+  AppComponent components; // Create scope Environment components
+  
+  /* create ApiControllers and add endpoints to router */
+  
+  auto router = components.httpRouter.getObject();
+  auto docEndpoints = oatpp::swagger::Controller::Endpoints::createShared();
+  
+  auto workController = WorkController::createShared();
+  workController->addEndpointsToRouter(router);
+  
+  docEndpoints->pushBackAll(workController->getEndpoints());
+  
+  auto swaggerController = oatpp::swagger::Controller::createShared(docEndpoints);
+  swaggerController->addEndpointsToRouter(router);
+
+  auto staticController = StaticController::createShared();
+  staticController->addEndpointsToRouter(router);
+
+  /* Get connection handler component */
+  OATPP_COMPONENT(std::shared_ptr<oatpp::network::ConnectionHandler>, connectionHandler);
+
+  /* Get connection provider component */
+  OATPP_COMPONENT(std::shared_ptr<oatpp::network::ServerConnectionProvider>, connectionProvider);
+
+  /* create server */
+  oatpp::network::Server server(connectionProvider,
+                                connectionHandler);
+  
+  OATPP_LOGD("Server", "Running on port %s...", connectionProvider->getProperty("port").toString()->c_str());
+  
+  server.run();
+
+  /* stop db connection pool */
+  OATPP_COMPONENT(std::shared_ptr<oatpp::provider::Provider<oatpp::sqlite::Connection>>, dbConnectionProvider);
+  dbConnectionProvider->stop();
+  
+}
+
+/**
+ *  main
+ */
+int main(int argc, const char * argv[]) {
+
+  oatpp::base::Environment::init();
+
+  run();
+  
+  /* Print how much objects were created during app running, and what have left-probably leaked */
+  /* Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance */
+  std::cout << "\nEnvironment:\n";
+  std::cout << "objectsCount = " << oatpp::base::Environment::getObjectsCount() << "\n";
+  std::cout << "objectsCreated = " << oatpp::base::Environment::getObjectsCreated() << "\n\n";
+  
+  oatpp::base::Environment::destroy();
+  
+  return 0;
+}
