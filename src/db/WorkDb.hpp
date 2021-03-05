f93b2174a5f2789d061e18e0786e63c363399082
aydinguler <69461459+aydinguler@users.noreply.github.com>
2021-03-05 11:53:06 +0300

    Create database client definitions

---
+++ src/db/WorkDb.hpp
@@ -0,0 +1,62 @@
+
+#ifndef CRUD_WORKDB_HPP
+#define CRUD_WORKDB_HPP
+
+#include "dto/WorkDto.hpp"
+#include "oatpp-sqlite/orm.hpp"
+
+#include OATPP_CODEGEN_BEGIN(DbClient) //<- Begin Codegen
+
+/**
+ * WorkDb client definitions.
+ */
+class WorkDb : public oatpp::orm::DbClient {
+public:
+
+    WorkDb(const std::shared_ptr<oatpp::orm::Executor>& executor)
+        : oatpp::orm::DbClient(executor)
+    {
+
+        oatpp::orm::SchemaMigration migration(executor);
+        migration.addFile(1 /* start from version 1 */, DATABASE_MIGRATIONS "/001_init.sql");
+        // TODO - Add more migrations here.
+        migration.migrate(); // <-- run migrations. This guy will throw on error.
+
+        auto version = executor->getSchemaVersion();
+        OATPP_LOGD("WorkDb", "Migration - OK. Version=%d.", version);
+
+    }
+
+        QUERY(createWork,
+            "INSERT INTO AppWork"
+            "(taskName, taskState) VALUES "
+            "(:work.taskName, :work.taskState);",
+            PARAM(oatpp::Object<WorkDto>, work))
+
+        QUERY(ChangeState,
+            "UPDATE AppWork "
+            "SET "
+            " taskName=:work.taskName, "
+            " taskState=:work.taskState "
+            "WHERE "
+            " taskID=:work.taskID;",
+            PARAM(oatpp::Object<WorkDto>, work))
+
+        QUERY(getWorkById,
+            "SELECT * FROM Appwork WHERE taskID=:taskID;",
+            PARAM(oatpp::Int32, taskID))
+
+        QUERY(getAllWorks,
+            "SELECT * FROM AppWork LIMIT :limit OFFSET :offset;",
+            PARAM(oatpp::UInt32, offset),
+            PARAM(oatpp::UInt32, limit))
+
+        QUERY(deleteWorkById,
+            "DELETE FROM AppWork WHERE taskID=:taskID;",
+            PARAM(oatpp::Int32, taskID))
+
+};
+
+#include OATPP_CODEGEN_END(DbClient) //<- End Codegen
+
+#endif //CRUD_WORKDB_HPP
